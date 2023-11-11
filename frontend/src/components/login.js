b14b78ccc09fcfa65fe5114ff867c038fc128a04
Ilfat <redemax96@gmail.com>
2023-11-11 15:33:19 +0300

    add route

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,8 +1,8 @@
-(function (){
-    const Login = {
-        agreeElement: null,
-        processElement: null,
-        fields: [
+export class Form {
+    constructor() {
+        this.agreeElement = null;
+        this.processElement = null;
+        this.fields = [
             {
                 name: 'email',
                 id: 'email',
@@ -17,57 +17,57 @@
                 regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
                 valid: false
             },
-        ],
-        init() {
-            const that = this;
-            this.fields.forEach(item => {
-                item.element = document.getElementById(item.id);
-                item.element.onchange = function () {
-                    that.validateField.call(that, item, this);
-                }
-            });
+        ];
 
-            this.processElement = document.getElementById('process');
-            this.processElement.onclick = function () {
-                that.processForm()
+        const that = this;
+        this.fields.forEach(item => {
+            item.element = document.getElementById(item.id);
+            item.element.onchange = function () {
+                that.validateField.call(that, item, this);
             }
+        });
 
-            this.agreeElement = document.getElementById('flexCheckDefault');
-            this.agreeElement.onchange = function () {
-                that.validateForm();
-            }
-        },
-        validateField(field, element) {
-            if (!element.value || !element.value.match(field.regex)) {
-                element.style.setProperty("border", "1px solid red", "important")
-                field.valid = false;
-            } else {
-                element.removeAttribute('style');
-                field.valid = true;
-            }
-            this.validateForm();
-        },
-        validateForm() {
-            const validForm = this.fields.every(item => item.valid);
-            const isValid = this.agreeElement.checked && validForm;
-            if(isValid) {
-                this.processElement.removeAttribute('disabled');
-            } else {
-                this.processElement.setAttribute('disabled', 'disabled');
-            }
-            return isValid;
-        },
-        processForm() {
-            let paramString = '';
-            if (this.validateForm()) {
-                this.fields.forEach(item => {
-                    paramString += (!paramString ? '?' : '&') + item.name + '=' + item.element.value;
-                })
-
-                location.href = 'main.html' + paramString;
-            }
+        this.processElement = document.getElementById('process');
+        this.processElement.onclick = function () {
+            that.processForm()
         }
+
+        this.agreeElement = document.getElementById('flexCheckDefault');
+        this.agreeElement.onchange = function () {
+            that.validateForm();
+        }
+    }
+
+    validateField(field, element) {
+        if (!element.value || !element.value.match(field.regex)) {
+            element.style.setProperty("border", "1px solid red", "important")
+            field.valid = false;
+        } else {
+            element.removeAttribute('style');
+            field.valid = true;
+        }
+        this.validateForm();
     };
 
-    Login.init();
-})();
+    validateForm() {
+        const validForm = this.fields.every(item => item.valid);
+        const isValid = this.agreeElement.checked && validForm;
+        if (isValid) {
+            this.processElement.removeAttribute('disabled');
+        } else {
+            this.processElement.setAttribute('disabled', 'disabled');
+        }
+        return isValid;
+    };
+
+    processForm() {
+        let paramString = '';
+        if (this.validateForm()) {
+            this.fields.forEach(item => {
+                paramString += (!paramString ? '?' : '&') + item.name + '=' + item.element.value;
+            })
+
+            location.href = 'main.html' + paramString;
+        }
+    };
+}
---
+++ frontend/src/router.js
@@ -0,0 +1,78 @@
+import {Form} from "./components/login.js";
+
+export class Router {
+    constructor() {
+        this.routes = [
+            {
+                route: '#/',
+                title: 'Главная',
+                template: 'templates/main.html',
+                styles: 'styles/index.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/form',
+                title: 'Регистрация',
+                template: 'templates/signup.html',
+                styles: 'styles/login.css',
+                load: () => {
+                    new Form();
+                }
+            },
+            // {
+            //     route: '#/choice',
+            //     title: 'Выбор теста',
+            //     template: 'templates/choice.html',
+            //     styles: 'styles/choice.css',
+            //     load: () => {
+            //         new Choice();
+            //     }
+            // },
+            // {
+            //     route: '#/test',
+            //     title: 'Прохождение теста',
+            //     template: 'templates/test.html',
+            //     styles: 'styles/test.css',
+            //     load: () => {
+            //         new Test();
+            //     }
+            // },
+            // {
+            //     route: '#/result',
+            //     title: 'Результаты',
+            //     template: 'templates/result.html',
+            //     styles: 'styles/result.css',
+            //     load: () => {
+            //         new Result();
+            //     }
+            // },
+            // {
+            //     route: '#/right',
+            //     title: 'Правильные ответы',
+            //     template: 'templates/right.html',
+            //     styles: 'styles/right.css',
+            //     load: () => {
+            //         new Answers();
+            //     }
+            // },
+        ]
+    }
+    async openRoute() {
+        const newRoute = this.routes.find(item => {
+            return item.route === window.location.hash.split('?')[0];
+        })
+
+        if (!newRoute) {
+            window.location.href = '#/';
+            return;
+        }
+
+        document.getElementById('content').innerHTML =
+            await fetch(newRoute.template).then(response => response.text());
+        document.getElementById('styles').setAttribute('href', newRoute.styles);
+        document.getElementById('page-title').innerText = newRoute.title;
+        newRoute.load();
+    }
+}
